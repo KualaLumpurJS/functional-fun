b8e6cf3f052bbe48b85c32761b53f24d2ed819ab
Jonathan ES Lin <ernsheong@gmail.com>
2016-04-05 23:49:46 +0800

    Add more tests

--- test/test.js
+++ test/test.js
@@ -1,15 +1,18 @@
 import assert from 'assert';
 
 describe('JavaScript', function() {
+  // You can use your earlier defined functions in later tests!
+  let identity, add, mul, identityf, addf, applyf, curry, methodize, demethodize;
+
   describe('function', function() {
-    it('function parameter does not modify what the outside variable pointing to', () => {
+    it('function parameter does not modify what the outside variable is pointing to', () => {
       function func(o) {
         o = null;
       }
 
       let x = [];
       func(x);
-      assert.deepEqual(x, 'REPLACE ME (not necessarily a string)');
+      assert.deepEqual(x, undefined);
 
       function swap(a, b) {
         let temp = a;
@@ -17,13 +20,13 @@ describe('JavaScript', function() {
       }
       let a = 1, b = 2;
       swap(a, b);
-      assert.deepEqual(a, 'REPLACE ME (not necessarily a string)');
+      assert.deepEqual(a, undefined);
     });
   });
 
   describe('identity', () => {
     it('takes an argument and returns that argument when called', () => {
-      function identity(x) {
+      identity = function(a, b) {
         // YOUR CODE HERE
       }
 
@@ -33,7 +36,7 @@ describe('JavaScript', function() {
 
   describe('add', () => {
     it('adds two numbers', () => {
-      function add(a, b) {
+      add = function(a, b) {
         // YOUR CODE HERE
       }
 
@@ -43,7 +46,7 @@ describe('JavaScript', function() {
 
   describe('mul', () => {
     it('multiplies two numbers', () => {
-      function mul(a, b) {
+      mul = function(a, b) {
         // YOUR CODE HERE
       }
 
@@ -53,7 +56,7 @@ describe('JavaScript', function() {
 
   describe('identityf', () => {
     it('takes an argument and returns a function that returns that argument', () => {
-      function identityf(x) {
+      identityf = function(x) {
         // YOUR CODE HERE
       }
 
@@ -64,7 +67,7 @@ describe('JavaScript', function() {
 
   describe('addf', () => {
     it('adds from two invocations', () => {
-      function addf(x) {
+      addf = function(x) {
         // YOUR CODE HERE
       }
 
@@ -73,15 +76,68 @@ describe('JavaScript', function() {
   })
 
   describe('applyf', () => {
-    it ('takes a binary function, and makes it callable with two invocation', () => {
-      function applyf(f) {
+    it('takes a binary function, and makes it callable with two invocation', () => {
+      // a binary function takes two arguments
+      applyf = function(binary) {
         // YOUR CODE HERE
       }
 
-      let add = (a, b) => a + b;
-      let mul = (a, b) => a * b;
       assert.strictEqual(applyf(add)(3)(4), 7);
       assert.strictEqual(applyf(mul)(3)(4), 12);
+    })
+  })
+
+  describe('curry', () => {
+    it('takes a function and an argument, and returns a function that can supply a second argument', () => {
+      curry = function(binary, first) {
+        // YOUR CODE HERE
+      }
+
+      let add3 = curry(add, 3);
+      assert.strictEqual(add3(4), 7);
+      assert.strictEqual(curry(mul, 4)(6), 24);
+      // Wikipedia:
+      // In mathematics and computer science, currying is the technique of translating the
+      // evaluation of a function that takes multiple arguments (or a tuple of arguments) into evaluating a sequence of functions,
+      // each with a single argument.
+    })
+  })
+
+  describe('inc', () => {
+    it('takes an argument and increments it', () => {
+      // Write three ways to create the inc function *without writing any new functions*
+      const inc1 = undefined;
+      const inc2 = undefined;
+      const inc3 = undefined;
+
+      [inc1, inc2, inc3].forEach(inc => {
+        assert.strictEqual(inc(1), 2);
+        assert.strictEqual(inc(inc(1)), 3);
+      })
+    })
+  })
+
+  describe('methodize', () => {
+    it('converts a binary function to a method', () => {
+      methodize = function(binary) {
+        // YOUR CODE HERE
+      }
+      // add takes two arguments, e.g. add(1, 2) returns 3A
+      // methodize "puts" one of the arguments in front (as the caller), taking in just one argument instead
+
+      Number.prototype.add = methodize(add);
+      assert.strictEqual((3).add(4), 7);
+    })
+  })
+  
+  describe('demethodize', () => {
+    it('converts a method to a binary function', () => {
+      demethodize = function(method) {
+        // YOUR CODE HERE
+      }
+
+      Number.prototype.add = methodize(add);
+      assert.strictEqual(demethodize(Number.prototype.add)(5, 6), 11);
     })
   })
 });
